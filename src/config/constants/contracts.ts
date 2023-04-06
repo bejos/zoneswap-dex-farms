e0ab71f15907c95e1f66270b383e15fc0d9fab7b
Mlbb Fan <77885074+retjehnetwork@users.noreply.github.com>
2023-04-07 00:48:36 +0700

    Address

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,27 +1,27 @@
 export default {
   cake: {
-    56: '0x81e032c97a5cbbf06851412e2b1adcb2b51de9d1',
+    56: '0x1a639e150d2210a4be4a5f0857a9151b241e7ae4',
     97: '',
   },
   masterChef: {
-    56: '0x92b0d8b2435bd8bb5d6721b9fa33029b76b248f4',
+    56: '0xdbf74f167a4e0b97a072c7ed51df6c6e8ec0353b',
     97: '',
   },
   wbnb: {
-    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    56: '0x40375c92d9faf44d2f9db9bd9ba41a3317a2404f',
     97: '',
   },
   lottery: {
-    56: '',
+    56: '0x50E298C8DA58c3D7BfE040d9e3a10E62F4bc44D8',
     97: '',
   },
   lotteryNFT: {
-    56: '',
+    56: '0xFaF04A8e6B19c5574e220b5FC55348b7ef14E769',
     97: '',
   },
   mulltiCall: {
-    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
-    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    56: '0x40fcd694c9ebbc46a2230ad498fbde11ae0111a8',
+    97: '',
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
